aa6412fdf7b3645305e3f23f1f8b7ab31044b59d
23lavi <reislavi28@gmail.com>
2025-05-27 21:24:42 -0300

    teste do git push

--- src/components/Exemplo1.jsx
+++ src/components/Exemplo1.jsx
@@ -2,6 +2,7 @@ export function Exemplo1(){
     return (
         <section>
             <h2>Exemplo 1: utilizações de useEffect</h2>
+            <h1>teste do git push</h1>
         </section>
     )
 }
